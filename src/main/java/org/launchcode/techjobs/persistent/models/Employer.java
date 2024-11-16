c6d79c4c2b2b3d04ca269ef2cfe4317c8560c540
evaluc <ealuckow@gmail.com>
2024-11-15 23:56:26 -0600

    Complete Models section of Task 2, 6 tests fail, 12 pass

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,20 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "Location is required")
+    @Size(max = 256, message = "Location has a maximum size of 256 characters")
+    private String location;
+
+    public void setLocation(String location) {this.location = location; }
+
+    public String getLocation() {return location; }
+
+    //no-arg constructor required for Entity
+    public Employer() {}
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,21 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @NotBlank(message = "Description is required")
+    @Size(max = 512, message = "Description has a maximum size of 512 characters")
+    private String description;
+
+    public void setDescription(String description) {this.description = description; }
+
+    public String getDescription() {return description; }
+
+    //no-arg constructor required for Entity
+    public Skill() {}
 
 }
